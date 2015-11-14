60fb25353ed78cd71fc2f59ca2846f4750c50c23
Anton Serra <aserratorta@gmail.com>
2015-11-14 12:24:24 +0100

    Google API Bundle step 2

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Apoutchika\LoremIpsumBundle\ApoutchikaLoremIpsumBundle(),
+            new HappyR\Google\ApiBundle\HappyRGoogleApiBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
