6eba3e879c7680be610f60d61053e948dbdf5437
Anton Serra <aserratorta@gmail.com>
2015-11-14 13:10:02 +0100

    IvoryGoogleMapBundle install the bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Apoutchika\LoremIpsumBundle\ApoutchikaLoremIpsumBundle(),
-            new HappyR\Google\ApiBundle\HappyRGoogleApiBundle(),
+            new Ivory\GoogleMapBundle\IvoryGoogleMapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
