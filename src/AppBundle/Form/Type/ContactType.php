f07b3e662be7c3c93296bc0338ef788d626776bf
David Romaní <david@flux.cat>
2015-11-21 20:32:30 +0100

    add form translation messages

--- src/AppBundle/Form/Type/ContactType.php
+++ src/AppBundle/Form/Type/ContactType.php
@@ -10,26 +10,45 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', 'text', array(
-                'label' => 'NOM',
-                'required' => true,
-            ))
-            ->add('email', 'email', array(
-                'label' => 'EMAIL',
-                'required' => true,
-            ))
-            ->add('phone', 'text', array(
-                'label' => 'TELEFON',
-                'required' => false,
-            ))
-            ->add('message', 'textarea', array(
-                'label' => 'MISSATGE',
-                'required' => false,
-            ))
-            ->add('send', 'submit', array(
-                'label' => 'ENVIAR',
-            ))
-        ;
+            ->add(
+                'name',
+                'text',
+                array(
+                    'label'    => 'frontend.index.contact.form.name',
+                    'required' => true,
+                )
+            )
+            ->add(
+                'email',
+                'email',
+                array(
+                    'label'    => 'frontend.index.contact.form.email',
+                    'required' => true,
+                )
+            )
+            ->add(
+                'phone',
+                'text',
+                array(
+                    'label'    => 'frontend.index.contact.form.phone',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'message',
+                'textarea',
+                array(
+                    'label'    => 'frontend.index.contact.form.message',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'send',
+                'submit',
+                array(
+                    'label' => 'frontend.index.contact.form.submit',
+                )
+            );
     }
 
     public function getName()
