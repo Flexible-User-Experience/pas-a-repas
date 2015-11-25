d4bc3c79acdcd96b91bf448b12239c9e1b1a9eb9
Anton Serra <aserratorta@gmail.com>
2015-11-25 16:41:34 +0100

    install fixtures bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Apoutchika\LoremIpsumBundle\ApoutchikaLoremIpsumBundle(),
             new Ivory\GoogleMapBundle\IvoryGoogleMapBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
