c7a8d6e4d12909e4e5048a6628711bb2cbbb98e7
Anton Serra <aserratorta@gmail.com>
2015-11-25 16:20:12 +0100

    create category fixture

---
+++ src/AppBundle/DataFixtures/ORM/Categories.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\Category;
+
+
+class Categories implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $categories = array(
+            'Prova1',
+            'Prova2',
+            'Prova3',
+            'Prova4',
+            'Prova5',
+        );
+
+        foreach ($categories as $title) {
+            $category = new Category();
+            $category->setTitle($title);
+
+            $manager->persist($category);
+        }
+
+        $manager->flush();
+    }
+}
