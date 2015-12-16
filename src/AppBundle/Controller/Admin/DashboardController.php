9be6d36bb39ddd971c6466508dce51ef131ae406
Anton Serra <aserratorta@gmail.com>
2015-12-17 00:30:57 +0100

    Show latest messages #23

--- src/AppBundle/Controller/Admin/DashboardController.php
+++ src/AppBundle/Controller/Admin/DashboardController.php
@@ -28,17 +28,42 @@ class DashboardController extends Controller
     public function dashboardAction()
     {
         $contacts = $this->getDoctrine()->getRepository('AppBundle:Contact')->findAll();
+        $posts = $this->getDoctrine()->getRepository('AppBundle:Post')->findAll();
+        $categories = $this->getDoctrine()->getRepository('AppBundle:Category')->findAll();
+
 
         $contactMsgsAmount = 0;
+        $postAmount = 0;
+        $categoryAmount = 0;
         $today = new \DateTime('today');
 
-        foreach ($contacts as $contact) {
-            if ($contact->getDate()->format('m') == $today->format('m') && $contact->getDate()->format('Y') == $today->format('Y')) {
+        foreach ($contacts as $contact)
+        {
+            if ($contact->getDate()->format('m') == $today->format('m') && $contact->getDate()->format('Y') == $today->format('Y'))
+            {
                 $contactMsgsAmount = $contactMsgsAmount + 1;
             }
         }
 
-        return $this->render('Admin/dashboard.html.twig', array ('contactMsgsAmount' => $contactMsgsAmount));
-    }
+        foreach ($posts as $post)
+        {
+             if ($post->getCreatedDate()->format('m') == $today->format('m') && $post->getCreatedDate()->format('Y') == $today->format('Y'))
+             {
+                $postAmount = $postAmount + 1;
+             }
+        }
 
-}
+        foreach ($categories as $category)
+        {
+            if ($category->getCreatedDate()->format('m') == $today->format('m') && $category->getCreatedDate()->format('Y') == $today->format('Y'))
+            {
+                $categoryAmount = $categoryAmount + 1;
+            }
+        }
+
+        return $this->render('Admin/dashboard.html.twig', array(
+                'contactMsgsAmount' => $contactMsgsAmount,
+                'postAmount' => $postAmount,
+                'categoryAmount' => $categoryAmount));
+        }
+    }
