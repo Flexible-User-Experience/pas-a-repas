359d87c2bc0a560bd6b4bc9abe158faf4d5509ec
Anton Serra <aserratorta@gmail.com>
2015-12-18 04:59:05 +0100

    Install GenemuFormBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Liip\ImagineBundle\LiipImagineBundle(),
             new SC\DatetimepickerBundle\SCDatetimepickerBundle(),
+            new Genemu\Bundle\FormBundle\GenemuFormBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
