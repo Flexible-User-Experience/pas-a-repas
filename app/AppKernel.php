0d620329af99ed8420dd8c4c0ee17b8eec981fdf
Anton Serra <aserratorta@gmail.com>
2015-12-18 16:41:25 +0100

    Use file uploads VichUploaderBundle #27

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel
             new Liip\ImagineBundle\LiipImagineBundle(),
             new SC\DatetimepickerBundle\SCDatetimepickerBundle(),
             new Genemu\Bundle\FormBundle\GenemuFormBundle(),
+            new Vich\UploaderBundle\VichUploaderBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
