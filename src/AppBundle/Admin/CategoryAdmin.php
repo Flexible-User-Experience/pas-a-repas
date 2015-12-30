af9378470b56f2b4b14ef0740e4cb85e7af5597d
David Romaní <david@flux.cat>
2015-12-30 19:56:17 +0100

    #31 improve categories admin 1

---
+++ src/AppBundle/Admin/CategoryAdmin.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+/**
+ * Class CategoryAdmin
+ *
+ * @category Admin
+ * @package  AppBundle\Admin
+ * @author   David Romaní <david@flux.cat>
+ */
+class CategoryAdmin extends Admin
+{
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('title')
+            ->add('enabled')
+        ;
+    }
+
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('title')
+            ->add('enabled')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('title')
+            ->add('enabled', null, array(
+                'editable' => true,
+            ))
+        ;
+    }
+}
