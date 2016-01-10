6bef9d47e6797ec57e5f87c7da2ef3f6dce84e05
Anton Serra <aserratorta@gmail.com>
2016-01-10 04:00:14 +0100

    add Receipt entity and repo

---
+++ src/AppBundle/Entity/Receipt.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use AppBundle\Entity\Traits\DateTrait;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * Receipt Entity Class
+ *
+ * @category Entity
+ * @package  AppBundle\Entity
+ * @author   Anton Serra <aserratorta@gmail.com>
+ *
+ * @ORM\Table(name="rebut")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ReceiptRepository")
+ */
+class Receipt extends Base
+{
+    use DateTrait;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(type="integer", name="fecha_pago", nullable=true)
+     */
+    private $payDay;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", name="tipo")
+     */
+    private $type;
+
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(type="float", name="import")
+     */
+    private $amount;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="integer", name="cobrat", nullable=true)
+     */
+    private $collected;
+
+    /**
+     * @var Customer
+     *
+     * @ORM\ManyToOne(targetEntity="Customer", inversedBy="receipts")
+     * @ORM\JoinColumn(name="client_id", referencedColumnName="id")
+     */
+    private $customer;
+
+
+     /**
+     * To string
+     *
+     * @return string
+     */
+    public function __toString() {
+
+        return $this->amount ? $this->getAmount().' '.$this->getCustomer() : '---';
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getPayDay()
+    {
+        return $this->payDay;
+    }
+
+    /**
+     * @param \DateTime $payDay
+     * @return Receipt
+     */
+    public function setPayDay($payDay)
+    {
+        $this->payDay = $payDay;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     * @return Receipt
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @param float $amount
+     * @return Receipt
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+        return $this;
+    }
+
+    /**
+     * @return Customer
+     */
+    public function getCustomer()
+    {
+        return $this->customer;
+    }
+
+    /**
+     * @param Customer $customer
+     * @return Receipt
+     */
+    public function setCustomer($customer)
+    {
+        $this->customer = $customer;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCollected()
+    {
+        return $this->collected;
+    }
+
+    /**
+     * @param int $collected
+     * @return Receipt
+     */
+    public function setCollected($collected)
+    {
+        $this->collected = $collected;
+        return $this;
+    }
+
+}
---
+++ src/AppBundle/Repository/ReceiptRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class ReceiptRepository
+ *
+ * @category Repository
+ * @package  AppBundle\Repository
+ * @author   Anton Serra <aserratorta@gmail.com>
+ */
+class ReceiptRepository extends EntityRepository
+{
+
+}
