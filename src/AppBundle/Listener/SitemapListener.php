d27cd87567ce40ee437413c59e34a5ade05ae1d5
Anton Serra <aserratorta@gmail.com>
2016-02-17 18:19:30 +0100

    fix SitemapListener2

--- src/AppBundle/Listener/SitemapListener.php
+++ src/AppBundle/Listener/SitemapListener.php
@@ -89,6 +89,28 @@ class SitemapListener implements SitemapListenerInterface
             }
             /** @var Post $post */
             foreach ($this->posts as $post) {
+//            foreach ($this->posts as $post) {
+                $url = $this->router->generate(
+                    'blog',
+                    array(
+                        'slug' => $post->getSlug(),
+                    ),
+                    UrlGeneratorInterface::ABSOLUTE_URL
+                );
+                $event
+                    ->getGenerator()
+                    ->addUrl(
+                        new UrlConcrete(
+                            $url,
+                            new \DateTime(),
+                            UrlConcrete::CHANGEFREQ_HOURLY,
+                            1
+                        ),
+                        'default'
+                    );
+            }
+            /** @var Post $post */
+            foreach ($this->posts as $post) {
                 $url = $this->router->generate(
                     'blog_detail',
                     array(
