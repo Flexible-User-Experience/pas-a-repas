14206e970c2cc403586eae388e056a606efaa8c0
Anton Serra <aserratorta@gmail.com>
2016-02-23 22:44:40 +0100

    create CreditsController

---
+++ src/AppBundle/Controller/Frontend/CreditsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Controller\Frontend;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class CreditsController
+ *
+ * @category Controller
+ * @package  AppBundle\Controller\Frontend
+ * @author   Anton Serra <aserratorta@gmail.com>
+ */
+class CreditsController extends Controller
+{
+    /**
+     * @Route("/credits", name="credits")
+     */
+    public function creditsAction()
+    {
+        return $this->render('Front/Web/credits.html.twig', array(
+        ));
+    }
+}
